8500630c21d73162db7a3041f649bb572465b420
Robert <robert@kokos.one>
2018-11-20 14:06:52 +0200

    conversion map

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ var assetsDecs = {eth: 1e18,
                   doge: 1e8,
                   btc: 1e8};
 
+function toBits(asset, amount){
+    return (new BigNumber(amount)).multipliedBy(assetsDecs[asset]).integerValue().toString();
+}
+function toUnit(asset, amount){
+    return (new BigNumber(amount)).dividedBy(assetsDecs[asset]).toFixed(8)
+}
+
+
 
 function convert(scope, fromAsset, fromAssetVolume,
-                 toAsset, toAssetVolume){
+                 toAsset, toAssetVolume, position){
     return new Promise(function(resolve, reject){
-      
+        if(typeof(position) == "undefined"
+           || position == null){
+            position = 0;
+        }
+        
     });
 
 }
@@ -27,7 +39,7 @@ function balance(scope, asset, position){
         scope.wallet.positionBalance(asset, position)
             .then(function(w){
                 resolve({address: w.address.address,
-                         balance: (new BigNumber(w.total)).dividedBy(assetsDecs[w.address.symbol]).toFixed(8),
+                         balance: toBits(asset, w.total),
                          symbol: w.address.symbol,
                          network: w.address.network,
                          position: w.address.position});
@@ -54,7 +66,20 @@ function transfer(scope, asset, amount, toAddress, position){
         }
         balance(scope, asset, position)
             .then(function(b){
-                
+                var bn = new BigNumber(b);
+                if(!bn.isGreaterThan(amount)){
+                    return reject({error: "Not Enough Balance"});
+                }
+                scope.wallet.sendFromPosition(asset, position,
+                                              toBits(asset, amount),
+                                              toAddress, 0, 0, b.address)
+                    .then(function(r){
+                        resolve({asset: asset,
+                                 from: b.address,
+                                 fromPosition: position,
+                                 amount: amount,
+                                 to: toAddress});
+                    }).catch(reject);
             }).catch(reject);
 
     });
@@ -68,9 +93,27 @@ function define(config){
         scope.account = Wallet.createAccount(config.wallet.passphrase, 0, 0);
      
         scope.network = config.wallet.network;
-        scope.wallet = Wallet.start(scope.account, config.wallet.network);
+        scope.wallet = Wallet.start(scope.account,
+                                    config.wallet.network);
         scope.api.pairs()
             .then(function(pairs){
+                scope.cache.conversionMap = {};
+                for(var pairSymbol in pairs){
+                    var pair = pairs[pairSymbol];
+                    if(typeof(scope.cache.conversionMap[pair.base_asset]) == "undefined"){
+                        scope.cache.conversionMap[pair.base_asset] = {};
+                    }
+                    if(typeof(scope.cache.conversionMap[pair.quote_asset]) == "undefined"){
+                        scope.cache.conversionMap[pair.quote_asset] = {};
+                    }
+
+                    scope.cache.conversionMap[pair.base_asset][pair.quote_asset] = {side: "ask",
+                                                                                    pair: pairSymbol};
+                    scope.cache.conversionMap[pair.quote_asset][pair.base_asset] = {side: "bid",
+                                                                                    pair: pairSymbol};
+                    
+                    
+                }
                 scope.cache.pairs = pairs;
                 resolve({convert: _.partial(convert, scope),
                          balance: _.partial(balance, scope)});
