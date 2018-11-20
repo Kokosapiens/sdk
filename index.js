0a290254d07cab735591c1f54960e9bfdd9b98fe
Robert <robert@kokos.one>
2018-11-20 11:41:12 +0200

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+var Api = require("@kokosapiens/api");
+var Wallet = require("@kokosapiens/wallet");
+var _ = require("lodash");
+var cache = {};
+function convert(scope, fromAsset, fromAssetVolume,
+                 toAsset, toAssetVolume){
+
+
+}
+
+function balance(scope, asset, position){
+
+    return new Promise(function(resolve, reject){
+        if(typeof(position) == "undefined"
+           || position == null){
+            position = 0;
+        }
+        scope.wallet.positionBalance(asset, position).then(resolve).catch(reject);
+    });
+}
+
+function address(scope, asset, position){
+    return new Promise(function(resolve, reject){
+        if(typeof(position) == "undefined"
+           || position == null){
+            position = 0;
+        }
+        scope.wallet.positionBalance(asset, position).then(resolve).catch(reject);
+    });
+}
+
+function define(config){
+    var scope = {};
+    scope.api = Api(config.api);
+    scope.wallet = Wallet(config.wallet.passphrase);
+    scope.api.pairs()
+        .then(function(pairs){
+            console.log(pairs);
+            cache.pairs = pairs;
+        }).catch(function(){
+
+        });
+    return {convert: _.partial(convert, scope),
+            balance: _.partial(balance, scope)};
+}
+
+module.exports = define;
